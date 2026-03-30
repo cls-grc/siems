a72578c2f794a0a9defd377db4d1636704d19c3e
cls-grc <celsigarcia036@gmail.com>
2026-03-30 08:24:03 +0800

    Delete siems/payment(sub6) directory

--- siems/payment(sub6)/check_audit.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config/db_connect.php';
-
-$stmt = $pdo->query("SELECT * FROM audit_log");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-
-$stmt = $pdo->query("SHOW TRIGGERS");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
--- siems/payment(sub6)/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-$_SESSION['message'] = 'You have been logged out successfully.';
-$_SESSION['msg_type'] = 'info';
-header('Location: index.php');
-exit;
-?>
